10081039ccbb9eee53a33f47003e5a3de8f49065
Victor Le Pen <victorlepen@Victors-Air.home>
2022-09-30 16:46:42 +0100

    fix tailwind

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './index.css';
+import './styles/app.css';
+import './assets/tailwind.css';
 // Vue.config.productionTip = false
 
 // new Vue({
