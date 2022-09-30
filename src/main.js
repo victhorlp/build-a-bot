23fadc4637b9b90b909f52a07e0d794cec8b2be5
Victor Le Pen <victorlepen@Victors-Air.home>
2022-09-30 15:43:20 +0100

    Create vue proj + add tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './index.css';
+// Vue.config.productionTip = false
+
+// new Vue({
+//     render: h => h(App),
+// }).$mount('#app')
 
 createApp(App).mount('#app');
